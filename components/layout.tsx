df75234388e4a916bcd6b8688ecc045aa844b03c
Alok Prateek <i@alokprateek.in>
2022-07-09 02:56:20 +0530

    fix footer tips

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,7 +8,6 @@ import { ReactNode, useEffect, useRef, useState } from "react";
 import { useHotkeys } from "react-hotkeys-hook";
 import tools from "../public/tools.json";
 import MenuIcon from "../svg/menu.svg";
-import QuoteIcon from "../svg/quote.svg";
 import { ToolPageSettings, ToolPageSettingSchema } from "../utils/ToolPage";
 import Button from "./button";
 import styles from "./layout.module.scss";
@@ -206,24 +205,16 @@ export default function Layout<T extends ToolPageSettings>({
         {children}
         {panelVisibility.footer && (
           <>
-            <blockquote className={styles.blockquote}>
-              <div className={styles.blockquoteIcon}>
-                <QuoteIcon width="24" />
-              </div>
-              <span>HandyDev</span> is the best all-in-one toolbox for all
-              your needs...Handydev!
-              <footer>—— Sheldon Lee Cooper</footer>
-            </blockquote>
+            
             <footer className={styles.mainFooter}>
-              <nav>
-                <a href="https://github.com/thewhitewulfy/handydev/stargazers">
-                  Star on GitHub
-                </a>
-                <a href="https://github.com/thewhitewulfy/handydev/issues/new">
-                  Report a Bug
-                </a>
-                <Link href="/api-tips">API &amp; Tips</Link>
-              </nav>
+            <blockquote className={styles.blockquote}>
+            <ul>
+              <li> All editors are resizable, and the size preference will be persisted.</li>
+              <li> Press <code>/</code> to focus on the tool search input. Press{" "} <code>Enter</code> to select the first tool. </li>
+              <li> Press <code>⌘/CTRL + k</code> to open the setting dropdown if available. </li>
+              <li><Link href="/json?sidebar=off">Sidebar off</Link> | <Link href="/json?sidebar=off&amp;footer=off">Footer and Sidebar Off</Link> | <Link href="/json?line-numbers=off">Line Numbers Off</Link></li>
+            </ul>
+            </blockquote>
             </footer>
           </>
         )}
